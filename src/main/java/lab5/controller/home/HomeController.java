f3a3ef1a653f5865060a687b66a83fe9ec1bea87
rafael.cassiano <rafael.cassiano22@gmail.com>
2015-03-24 21:22:33 -0300

    Injetando o service no HomeBusiness

--- src/main/java/lab5/controller/home/HomeController.java
+++ src/main/java/lab5/controller/home/HomeController.java
@@ -1,8 +1,12 @@
 package lab5.controller.home;
 
+import lab5.business.HomeBusiness;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class HomeController {
-
+	@Autowired
+	private HomeBusiness business;
 }
